228dff19e2196f205af756a7cb0e3ec5cb13dbb9
SeoHoJun <ghwns6659@naver.com>
2020-06-10 22:57:33 +0900

    BFS & DFS

--- BFS & DFS/src/PermutationCycle/PermutationCycle.java
+++ BFS & DFS/src/PermutationCycle/PermutationCycle.java
@@ -3,6 +3,7 @@ package PermutationCycle;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class PermutationCycle {
@@ -15,6 +16,8 @@ public class PermutationCycle {
 		str = br.readLine();
 		StringTokenizer st = new StringTokenizer(str);
 		count = Integer.parseInt(st.nextToken());
+		int []count_size = new int[count];
+		
 		for(int i = 0; i < count; i++) {
 			str = br.readLine();
 			st = new StringTokenizer(str);
@@ -24,10 +27,38 @@ public class PermutationCycle {
 			
 			str = br.readLine();
 			st = new StringTokenizer(str);
-			for(int j = 0; j < size; j++) {
-				Cycle[j] = Integer.parseInt(st.nextToken());
+			// 입력한 순열 데이터를 배열에 삽입한다.
+			for(int j = 1; j <= size; j++) {
+				Cycle[j - 1] = Integer.parseInt(st.nextToken());
+			}
+			int j = 1;
+			int cycle_count = 0;
+			// 배열에 삽입된 데이터들을 가장 첫번째 데이터 부터 시작하여 순열 사이클의 갯수를 계산한다. 
+			boolean confirm = true;
+			while(confirm) {
+				if(flag[j - 1] == 0) {
+					flag[j - 1] = 1;
+					j = Cycle[j -  1];
+				}
+				else if(flag[j - 1] == 1 || flag[j - 1] > size) {
+					cycle_count++;
+					
+					confirm = Arrays.stream(flag).anyMatch(x -> x == 0);
+					if(confirm == true) {
+						for(int k = 1; k <= size; k++) {
+							if(flag[k - 1] == 0) {
+								j = k; 
+								break;
+							}
+						}
+					}
+					else count_size[i] = cycle_count;
+				}
 			}
 		}
 		
+		for(int i = 0; i < count; i++) {
+			System.out.println(count_size[i]);
+		}
 	}
 }
