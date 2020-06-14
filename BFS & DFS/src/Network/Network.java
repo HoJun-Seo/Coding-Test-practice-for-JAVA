5c02a0a80d7af80e85d48fec533e842180432696
SeoHoJun <ghwns6659@naver.com>
2020-06-14 22:39:45 +0900

    BFS & DFS

--- BFS & DFS/src/Network/Network.java
+++ BFS & DFS/src/Network/Network.java
@@ -1,9 +1,34 @@
 package Network;
 
+import java.util.Arrays;
+
 public class Network {
 	public int solution(int n, int[][] computers) {
         int answer = 0;
-        return answer;
+        for(int i = 0; i < computers.length; i++) {
+        	for(int j = 0; j < computers[i].length; j++) {
+        		if(computers[i][j] == 1) {
+        			if(i == j) {
+        				long count = Arrays.stream(computers[i]).filter(x -> x == 1 || x == -1).count();
+        				if(count == 1) answer++;
+        				
+        			}
+        			else if(computers[j][i] == 1) {
+        				boolean match = Arrays.stream(computers[i]).anyMatch(x -> x == -1);
+        				if(match == true) {
+        					computers[i][j] *= -1;
+            				computers[j][i] *= -1;
+        				}
+        				else {
+        					computers[i][j] *= -1;
+        					computers[j][i] *= -1;
+        					answer++;
+        				}
+        			}
+        		}
+        	}
+        }
+		return answer;
     }
 
 }
