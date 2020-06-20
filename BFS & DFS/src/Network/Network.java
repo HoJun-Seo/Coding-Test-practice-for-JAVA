33b99d45782f9001393b0afbc7ee84fd090e046f
SeoHoJun <ghwns6659@naver.com>
2020-06-20 23:40:57 +0900

    BFS & DFS

--- BFS & DFS/src/Network/Network.java
+++ BFS & DFS/src/Network/Network.java
@@ -2,31 +2,42 @@ package Network;
 
 import java.util.Arrays;
 
-public class Network {
+class Solution {
 	public int solution(int n, int[][] computers) {
         int answer = 0;
-        for(int i = 0; i < computers.length; i++) {
-        	for(int j = 0; j < computers[i].length; j++) {
+        for(int i = 0; i < n - 1; i++) {
+        	for(int j = 0; j < n; j++) {
         		if(computers[i][j] == 1) {
         			if(i == j) {
         				long count = Arrays.stream(computers[i]).filter(x -> x == 1 || x == -1).count();
-        				if(count == 1) answer++;
-        				continue;
+        				if(count == 1) {
+                            answer++;
+                            continue;
+                        }
+        				else continue;
         			}
         			long match = Arrays.stream(computers[i]).filter(x -> x == -1).count();
         			if(match > 0) {
+        				long match2 = Arrays.stream(computers[j]).filter(x -> x == -1).count();
+        				if(match2 > 0 && answer >= 2) {
+                            computers[i][j] *= -1;
+                            computers[j][i] *= -1;
+        					answer--;
+                            continue;
+        				}
         				computers[i][j] *= -1;
-            			computers[j][i] *= -1;
+                        computers[j][i] *= -1;
         			}
         			else {
         				computers[i][j] *= -1;
-        				computers[j][i] *= -1;
+                        computers[j][i] *= -1;
         				answer++;
         			}
         		}
         	}
         }
+        long count2 = Arrays.stream(computers[n - 1]).filter(x -> x == -1).count();
+        if(count2 == 0) answer++;
 		return answer;
     }
-
 }
