5d359a2ef2837a73f7d4de2ff5cc9fd4bdb7c100
SeoHoJun <ghwns6659@naver.com>
2020-06-23 23:55:42 +0900

    Update Network.java

--- BFS & DFS/src/Network/Network.java
+++ BFS & DFS/src/Network/Network.java
@@ -1,43 +1,55 @@
-package Network;
-
 import java.util.Arrays;
 
 class Solution {
 	public int solution(int n, int[][] computers) {
         int answer = 0;
-        for(int i = 0; i < n - 1; i++) {
-        	for(int j = 0; j < n; j++) {
-        		if(computers[i][j] == 1) {
-        			if(i == j) {
-        				long count = Arrays.stream(computers[i]).filter(x -> x == 1 || x == -1).count();
-        				if(count == 1) {
-                            			answer++;
-                            			continue;
-                        		}
-        				else continue;
-        			}
-        			long match = Arrays.stream(computers[i]).filter(x -> x == -1).count();
-        			if(match > 0) {
-        				long match2 = Arrays.stream(computers[j]).filter(x -> x == -1).count();
-        				if(match2 > 0 && answer >= 2) {
-                            			computers[i][j] *= -1;
-                            			computers[j][i] *= -1;
-        					answer--;
-                            			continue;
-        				}
-        				computers[i][j] *= -1;
-                        		computers[j][i] *= -1;
-        			}
-        			else {
-        				computers[i][j] *= -1;
-                        		computers[j][i] *= -1;
-        				answer++;
-        			}
+        long node = 0;
+        int[] flag = new int[n];
+        
+        for(int i = 0; i < n; i++) flag[i] = 1;
+        
+        for(int i = 0; i < n; i++) {
+        	long count = Arrays.stream(computers[i]).filter(x -> x == 1).count();
+        	node += count;
+        	if(count == 1) {
+        		flag[i] *= -1;
+        		answer++;
+        	}
+        }
+        node = (node - n) / 2;
+        int index1 = 0;
+        int i = 0;
+        
+        while(true) {
+        	if(i >= node) break;
+        	for(int index2 = 0; index2 < n; index2++) {
+        		if(computers[index1][index2] == 1 && index1 != index2) {
+        			computers[index1][index2] *= -1;
+        			computers[index2][index1] *= -1;
+                    if(flag[index1] == 1 && flag[index2] == 1) {
+                        flag[index1] *= -1; flag[index2] *= -1;
+                    }
+        			else if(flag[index1] == 1 && flag[index2] == -1) {
+                        flag[index1] *= -1;
+                    }
+                    else if(flag[index1] == -1 && flag[index2] == 1) {
+                        flag[index2] *= -1;
+                    }
+        			index1 = index2;
+        			index2 = 0; i++;
+        		}
+        	}
+        	answer++;
+        	
+        	for(int flagindex = 0; flagindex < n; flagindex++) {
+        		if(flag[flagindex] == 1) {
+        			index1 = flagindex;
+                    break;
         		}
         	}
         }
-        long count2 = Arrays.stream(computers[n - 1]).filter(x -> x == -1).count();
-        if(count2 == 0) answer++;
-	return answer;
+        
+        
+        return answer;
     }
 }
