7e55b705e07f1b27cd3f97a6f660cc2e968bbf64
SeoHoJun <ghwns6659@naver.com>
2020-07-01 18:48:48 +0900

    Update Solution.java

--- Stack & Queue/src/Bridge_Truck/Solution.java
+++ Stack & Queue/src/Bridge_Truck/Solution.java
@@ -1,30 +1,40 @@
 package Bridge_Truck;
 
 import java.util.LinkedList;
-/*2020/06/25 - 로직 재 설정중*/
+
 public class Solution {
 	public int solution(int bridge_length, int weight, int[] truck_weights) {
-		LinkedList<Integer> queue = new LinkedList<Integer>();
 		LinkedList<Integer> bridgequeue = new LinkedList<Integer>();
 		LinkedList<Integer> trucktime = new LinkedList<Integer>();
         int answer = 0;
-        int result = 0;
-        int time = 0;
+        int index = 0;
         
-        for(int i = 0; i < truck_weights.length; i++) { // 큐에 대기중인 트럭을 넣을 필요가 없을 듯 하다. 그냥 기존에 주어지는 배열을 바로 쓰도록 하자.
-        	queue.offer(truck_weights[i]);
+        if(weight >= truck_weights[index]) {
+        	 bridgequeue.offer(truck_weights[index]); // 다리위에 첫번째 트럭을 올려 둔 후 반복문 실행
+        	 weight -= truck_weights[index];
+        	 trucktime.offer(0);
+        	 trucktime.replaceAll(x -> x + 1);
+        	 index++;
+             answer++; 
         }
-        
-        if(!queue.isEmpty()) { // 대기중인 트럭이 있을 경우 조건
-        	//현재 다리가 견딜 수 있는 하중과 들어올 트럭의 무게를 비교하여 트럭을 다리위에 올릴지 말지를 결정한다.
-        	//하중의 판단은 트럭이 들어올 때는 트럭의 무게 만큼 하중의 무게를 빼고, 트럭이 빠져나가면 빠져나간 트럭의 무게만큼 하중의 무게를 더한다.
-        	//매 반복 회차마다 시간을 1초씩 더해가며 다리에서 빠져나간 트럭이 있을 경우 다리가 견딜 수 있는 하중을 빠져나간 트럭의 무게만큼 늘리고, 다리에 트럭이 들어 올 수 있다면 트럭의 무게만큼 다리가 견딜 수 있는 하중을 줄인다.
-        	//각 트럭이 들어올 때마다 트럭이 다리를 지나는 시간을 체크하여 다리의 길이만큼 해당 트럭이 이동하였다면 해당 트럭을 다리에서 빠져나오게끔 한다.
-        	// 다리위에 올라와 있는 각 트럭별 시간을 카운트 하기위한 자료구조(배열 또는 큐?), 대기중인 트럭을 표시할 자료구조(큐), 다리위를 지나가고 있는 트럭들을 위한 자료구조 가 필요할 것으로 판단된다.
-        	// //다리를 빠져나간 트럭을 위한 자료구조는 생성하지 않는다.
-        }
-        else { // 큐에 대기중인 트럭이 없을 경우 조건
+       
+        while(!bridgequeue.isEmpty()) {
+        	if(trucktime.peek() >= bridge_length) {
+        		trucktime.remove();
+        		weight += bridgequeue.peek();
+        		bridgequeue.remove();
+        	}
+        	if(index < truck_weights.length) {
+        		if(weight >= truck_weights[index]) {
+            		bridgequeue.offer(truck_weights[index]);
+            		weight -= truck_weights[index];
+            		trucktime.offer(0);
+            		index++;
+            	}
+        	}
+        	trucktime.replaceAll(x -> x + 1);
         	
+        	answer++; // 매 반복 마다 어떤 액션이 몇개가 실행되는 상관없이  무조건 시간은 1초씩만 더해진다.
         }
         return answer;
     }
