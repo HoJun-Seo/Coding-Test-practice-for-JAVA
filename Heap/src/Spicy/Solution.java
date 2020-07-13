51eb19c7176e6a04702ae7d80b87582bb970fe46
SeoHoJun <ghwns6659@naver.com>
2020-07-13 23:10:15 +0900

    Heap & PriorityQueue

---
+++ Heap/src/Spicy/Solution.java
@@ -0,0 +1,5 @@
+package Spicy;
+
+public class Solution {
+
+}
--- PracticeQuestion/src/Center_Character/Solution.java
+++ PracticeQuestion/src/Center_Character/Solution.java
@@ -1,5 +1,17 @@
 package Center_Character;
 
-public class Solution {
 
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        if(s.length() % 2 != 0) {
+        	int index = (s.length() / 2) + 1;
+        	answer = Character.toString(s.charAt(index));
+        }
+        else {
+        	int index = s.length() / 2;
+        	answer = s.substring(index, index+1);
+        }
+        return answer;
+    }
 }
