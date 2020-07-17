9ee1a58b762d69b2ba781aec6fd92a3664dd676f
SeoHoJun <ghwns6659@naver.com>
2020-07-18 01:13:19 +0900

    Update Solution.java

--- Stack & Queue/src/Stock/Solution.java
+++ Stack & Queue/src/Stock/Solution.java
@@ -1,25 +1,25 @@
 package Stock;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.stream.Stream;
+import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Queue<Integer> stock_queue = new LinkedList<Integer>();
-        for(int i = 0; i < prices.length; i++) stock_queue.add(prices[i]);
-        for(int i = 0; i < stock_queue.size()-1; i++) {
-        	int stock = stock_queue.poll();
-        	Object[] clone_queue= stock_queue.toArray();
-        	long count = Arrays.stream(clone_queue).filter(x -> (Integer)x < stock).count();
-        	if(count == 0) answer[i] = stock_queue.size();
-        	else {
-        		Stream<Object> lower = Arrays.stream(clone_queue).filter(x -> (Integer)x < stock);
-        		int index = Arrays.asList(clone_queue).indexOf(lower) + 1;
-        		answer[i] = index;
+        ArrayList<Integer> stock_stack = new ArrayList<Integer>();
+        for(int i = 0; i < prices.length-1; i++) {
+        	stock_stack.add(prices[i]);
+        	for(int j = i+1; j < prices.length; j++) {
+        		if(prices[j] >= stock_stack.get(0)) {
+        			stock_stack.add(prices[j]);
+        			answer[i] = stock_stack.size()-1;
+        		}
+        		else {
+        			answer[i] = stock_stack.size();
+        			stock_stack.clear();
+        			break;
+        		}
         	}
+        	stock_stack.clear();
         }
         
         return answer;
