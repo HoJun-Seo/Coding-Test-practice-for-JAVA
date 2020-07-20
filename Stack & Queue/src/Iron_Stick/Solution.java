c84b366bd21460ee8e562ee6fd3ae5d7505843d6
SeoHoJun <ghwns6659@naver.com>
2020-07-20 19:50:54 +0900

    Update Solution.java

--- Stack & Queue/src/Iron_Stick/Solution.java
+++ Stack & Queue/src/Iron_Stick/Solution.java
@@ -5,20 +5,15 @@ import java.util.Stack;
 class Solution {
     public int solution(String arrangement) {
         int answer = 0;
-        char[] iron = arrangement.toCharArray();
-        Stack<Character> iron_stack = new Stack<Character>();
+        arrangement = arrangement.replace("()", "L");
+        Stack<Integer> iron_stack = new Stack<Integer>();
         for(int i = 0; i < arrangement.length(); i++) {
-        	if(!iron_stack.isEmpty()) {
-        		char top = iron_stack.peek();
-        		if(iron[i] == ')' && top == '(') { // 레이저를 인식했을 경우
-        			
-        		}
-        		else if(iron[i] == ')' && top == ')') { // 쇠 막대기의 끝을 확인했을 경우
-        			
-        		}
-        		else iron_stack.push(iron[i]);
+        	if(arrangement.charAt(i) == '(') iron_stack.push(0);
+        	else if(!iron_stack.isEmpty() && arrangement.charAt(i) == 'L') iron_stack.replaceAll(x -> x + 1);
+        	else if(arrangement.charAt(i) == ')') {
+        		answer = iron_stack.peek() + 1;
+        		iron_stack.pop();
         	}
-        	else iron_stack.push(iron[i]);
         }
         return answer;
     }
