07dbb2e1b8f647db1d1838ae3cb9f99098c6f98b
SeoHoJun <ghwns6659@naver.com>
2020-07-24 23:52:02 +0900

    String process

---
+++ StringProcess/src/Character_Numbers/Main.java
@@ -0,0 +1,16 @@
+package Character_Numbers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		String[] str_num = str.trim().split(" ");
+		if(str.charAt(0) == ' ' && str.length() == 1) System.out.println(0);
+		else System.out.println(str_num.length);
+		br.close();
+	}
+}
