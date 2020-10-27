4448d61bcfd4b29db66d3b0fe770c8221e2c1dbb
SeoHoJun <ghwns6659@naver.com>
2020-10-27 23:58:51 +0900

    programmers_skillcheck

---
+++ StringProcess/src/Character_Numbers/Solution3.java
@@ -0,0 +1,12 @@
+package Character_Numbers;
+
+public class Solution3 {
+	public int solution(int n) {
+		int answer = 0;
+		String number = Integer.toString(n);
+		
+		
+		
+		return answer;
+	}
+}
