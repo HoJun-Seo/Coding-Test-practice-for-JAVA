7fbcc0e9803d6f47b0424651e068981bed173d4b
SeoHoJun <ghwns6659@naver.com>
2020-10-28 00:31:44 +0900

    programmers_skillcheck

--- StringProcess/src/Character_Numbers/Solution3.java
+++ StringProcess/src/Character_Numbers/Solution3.java
@@ -4,8 +4,12 @@ public class Solution3 {
 	public int solution(int n) {
 		int answer = 0;
 		String number = Integer.toString(n);
+		char[] number_array = new char[number.length()];
 		
-		
+		for(int i = 0; i < number.length(); i++) {
+			number_array[i] = number.charAt(i);
+			answer += number_array[i] - '0';
+		}
 		
 		return answer;
 	}
