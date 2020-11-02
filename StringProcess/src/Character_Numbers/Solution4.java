6fe9707d43f341759ebb3e7ee18ab448376b6f7f
SeoHoJun <ghwns6659@naver.com>
2020-11-03 00:29:31 +0900

    programmers_skillcheck

--- StringProcess/src/Character_Numbers/Solution4.java
+++ StringProcess/src/Character_Numbers/Solution4.java
@@ -8,18 +8,17 @@ public class Solution4 {
         
         String[] string_array = s.split(" ");
         for(int i = 0; i < string_array.length; i++) {
-        	for(int j = 0; j < string_array[i].length(); j++) {
-        		if(j % 2 == 0) {
-        			string_array[i] = string_array[i].replace(string_array[i].charAt(j), (char)(string_array[i].charAt(j) - 32));
-        			System.out.println(string_array[i].charAt(j));
-        		}	
+        	char[] char_array = string_array[i].toCharArray();
+        	for(int j = 0; j < char_array.length; j++) {
+        		if(j % 2 == 0 && char_array[j] >= 97 && char_array[j] <= 122) {
+        			char_array[j] = (char)(char_array[j] - 32);
+        		}
+        		
+        		else if(j % 2 == 0 && char_array[j] >= 65 && char_array[j] <= 90) {
+        			char_array[j] = (char)(char_array[j] + 32);
+        		}
         	}
-        	System.out.println(string_array[i]);
-        }
-        
-        for(int i = 0; i < string_array.length; i++) {
-        	answer += string_array[i];
-        	if(i != string_array.length - 1) answer += " ";
+        	answer += String.valueOf(char_array) + " ";
         }
         
         System.out.println(answer);
