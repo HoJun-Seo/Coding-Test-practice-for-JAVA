f71fed87449a028eeb120b0e08977c880ac8050e
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2023-03-15 15:34:19 +0900

    backjoon

--- BackJoon/src/BackJoonString/BlankKing.java
+++ BackJoon/src/BackJoonString/BlankKing.java
@@ -23,49 +23,39 @@ public class BlankKing {
 
             if (s.equals(""))
                 break;
-            // 문장의 첫 글자가 공백인 경우
-            if (s.charAt(0) == ' ') {
-                // 첫 글자 시작 인덱스를 찾는다.
-                int index = 0;
-                for (int i = 0; i < s.length(); i++) {
-                    if (s.charAt(i) != ' ') {
-                        index = i;
-                        break;
-                    }
-                }
-
-                s = s.substring(index, s.length()); // 첫 글자 앞 공백 제거
-            }
+            // 문장의 첫 글자가 공백인 경우 공백 제거
+            s = s.trim();
 
             // 문장에서 각 단어의 길이를 계산 한 후
             // 2차원 배열에 각 단어 저장
-            String str = "";
+
+            StringBuilder str = new StringBuilder();
             int index = 0;
             for (int i = 0; i < s.length(); i++) {
                 if (s.charAt(i) == ' ') {
-                    if (!str.equals("")) {
+                    if (str.length() != 0) {
                         // 문장에서 단어 하나를 완성한 상태
                         int size = str.length();
                         if (strSize[index] < size) { // 각 단어별 최대 길이를 저장
                             strSize[index] = size;
                         }
 
-                        array[inputCount][index] = str;
+                        array[inputCount][index] = str.toString();
                         index++;
-                        str = "";
+                        str.setLength(0);
                     }
                 } else {
-                    str += String.valueOf(s.charAt(i));
+                    str.append(String.valueOf(s.charAt(i)));
                 }
             }
 
             // 마지막 단어까지 2차원 배열의 각 행에 저장
-            if (!str.equals("")) {
+            if (str.length() != 0) {
                 int size = str.length();
                 if (strSize[index] < size) {
                     strSize[index] = size;
                 }
-                array[inputCount][index] = str;
+                array[inputCount][index] = str.toString();
             }
 
             inputCount++;
@@ -78,14 +68,14 @@ public class BlankKing {
                 break;
             else {
                 for (int j = 0; j < 180; j++) {
-                    String str = array[i][j];
 
-                    if (str == null) // 각 행의 단어들을 모두 본 상태라면
+                    if (array[i][j] == null) // 각 행의 단어들을 모두 본 상태라면
                         break;
                     else { // 각 행의 단어들을 계속 봐야 하는 경우
 
                         int size = strSize[j];
-
+                        StringBuilder str = new StringBuilder();
+                        str.append(array[i][j]);
                         // 현재단어의 길이가 각 열의 최대 길이 보다 작은 경우
                         if (str.length() <= size) {
 
@@ -93,16 +83,16 @@ public class BlankKing {
                                 int requireBlank = size - str.length();
                                 // 어차피 최대 길이면 requireBlank 가 0 이라서 반복문 실행이 안됨
                                 for (int x = 0; x < requireBlank; x++) {
-                                    str += " ";
+                                    str.append(" ");
                                 }
                                 // 단어간 구분을 위해 공백 하나 더 추가(최대길이 일때도 포함)
-                                str += " ";
+                                str.append(" ");
                             }
                             // 각 행의 마지막 단어인 경우
                             // 그냥 공백 추가 안 하면 됨
                         }
 
-                        array[i][j] = str;
+                        array[i][j] = str.toString();
                     }
                 }
             }
