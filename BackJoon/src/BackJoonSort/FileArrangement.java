90019a80e0f1c212ffa20de08946ec6a8eed3c60
HoJun-Seo <ghwns6659@gmail.com>
2023-03-16 23:52:52 +0900

    backjoon

---
+++ BackJoon/src/BackJoonSort/FileArrangement.java
@@ -0,0 +1,17 @@
+package BackJoonSort;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class FileArrangement {
+    public static void main(String[] args) {
+        new FileArrangement().solution();
+    }
+
+    private void solution() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+}
