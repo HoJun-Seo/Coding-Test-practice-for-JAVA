3f22602d758a56b308654d93fdda1ab912dae9de
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2023-03-28 18:05:57 +0900

    backjoon

--- BackJoon/src/BackJoonString/HTML.java
+++ BackJoon/src/BackJoonString/HTML.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class HTML {
     public static void main(String[] args) throws IOException {
@@ -16,31 +17,60 @@ public class HTML {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringBuilder sb = new StringBuilder();
+        int lineLength = 0;
         while (true) {
-            break;
-        }
-        if (sb.length() > 0) {
-            sb = new StringBuilder(sb.toString().trim());
-            bw.write(sb.toString() + "\n");
-        }
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-
-    private String singleBlank(String str) {
-        StringBuilder sb = new StringBuilder(str);
-        for (int i = 0; i < sb.length(); i++) {
-            if (sb.charAt(i) == ' ') {
-                for (int j = i + 1; j < sb.length(); j++) {
-                    if (sb.charAt(j) == ' ') {
-                        sb.setCharAt(j, '*');
-                    } else
-                        break;
+            String str = br.readLine();
+            if (str == null)
+                break;
+            else if (str.equals(""))
+                continue;
+            else {
+                StringTokenizer st = new StringTokenizer(str, " \t\n");
+                String[] input = new String[st.countTokens()];
+                int index = 0;
+                while (st.hasMoreTokens()) {
+                    input[index] = st.nextToken();
+                    index++;
+                }
+                for (String word : input) {
+                    if (!word.equals("<br>") && !word.equals("<hr>")) {
+                        if (lineLength == 0) {
+                            sb.append(word);
+                            lineLength = word.length();
+                        } else if (lineLength + word.length() < 80) {
+                            sb.append(" " + word);
+                            lineLength += word.length() + 1;
+                        } else {
+                            sb.append("\n" + word);
+                            lineLength = word.length();
+                        }
+                    } else {
+                        if (word.equals("<br>")) {
+                            sb.append("\n");
+                            lineLength = 0;
+                        } else if (word.equals("<hr>")) {
+                            if (lineLength == 0) {
+                                for (int i = 0; i < 80; i++) {
+                                    sb.append("-");
+                                }
+                            } else {
+                                lineLength = 0;
+                                sb.append("\n");
+                                for (int i = 0; i < 80; i++) {
+                                    sb.append("-");
+                                }
+                            }
+                            sb.append("\n");
+                        }
+                    }
                 }
             }
         }
-        str = sb.toString().replaceAll("\\*", "");
-        return str;
+
+        sb.append("\n");
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
