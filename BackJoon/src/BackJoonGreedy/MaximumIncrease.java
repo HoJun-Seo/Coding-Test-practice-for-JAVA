18650eea8df1e3a56971553c893e1b42cb46dea5
HoJun-Seo <ghwns6659@gmail.com>
2023-03-30 23:41:32 +0900

    backjoon

--- BackJoon/src/BackJoonGreedy/MaximumIncrease.java
+++ BackJoon/src/BackJoonGreedy/MaximumIncrease.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class MaximumIncrease {
     public static void main(String[] args) throws NumberFormatException, IOException {
@@ -14,30 +15,18 @@ public class MaximumIncrease {
     private void solution() throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int count = Integer.parseInt(br.readLine());
-        int[] array = new int[count];
-        String[] input = br.readLine().split(" ");
-        for (int i = 0; i < count; i++) {
-            array[i] = Integer.parseInt(input[i]);
+        
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int answer = 0;
+        int min = Integer.MAX_VALUE;
+        while (n-->0) {
+            int cur = Integer.parseInt(st.nextToken());
+            answer = Math.max(answer, cur - min);
+            min = Math.min(min, cur);
         }
 
-        int result = 0;
-        int minIndex = 0;
-        int number = array[0];
-
-        for (int i = 1; i < array.length; i++) {
-            if (number > array[i]) {
-                number = array[i];
-                minIndex = i;
-            }
-        }
-        for (int i = minIndex + 1; i < array.length; i++) {
-            if (array[i] > number) {
-                result = Math.max(result, array[i] - number);
-            }
-        }
-        bw.write(result + "\n");
+        bw.write(answer + "\n");
         bw.flush();
         bw.close();
         br.close();
