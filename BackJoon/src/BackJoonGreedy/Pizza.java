8e19462401141c0d1e10ed2d25855d722d102aaf
HoJun-Seo <ghwns6659@gmail.com>
2023-04-01 23:49:04 +0900

    backjoon

---
+++ BackJoon/src/BackJoonGreedy/Pizza.java
@@ -0,0 +1,19 @@
+package BackJoonGreedy;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Pizza {
+    public static void main(String[] args) {
+        new Pizza().solution();
+    }
+
+    private void solution() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int count = Integer.parseInt(br.readLine());
+    }
+}
