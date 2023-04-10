62e75457f4bab01d7f414bd7885bd95bd7237fce
HoJun-Seo <ghwns6659@gmail.com>
2023-04-10 21:54:12 +0900

    backjoon

---
+++ BackJoon/src/BackJoonGreedy/BreakPot.java
@@ -0,0 +1,17 @@
+package BackJoonGreedy;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BreakPot {
+    public static void main(String[] args) {
+        new BreakPot().solution();
+    }
+
+    private void solution() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+}
