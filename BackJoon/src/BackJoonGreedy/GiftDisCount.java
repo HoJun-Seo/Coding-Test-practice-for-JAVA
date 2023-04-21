72a38e39a78511d686d2fe082d6e0f0cdea3ce4b
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2023-04-21 18:00:19 +0900

    backjoon

--- BackJoon/src/BackJoonGreedy/GiftDisCount.java
+++ BackJoon/src/BackJoonGreedy/GiftDisCount.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.PriorityQueue;
 
 public class GiftDisCount {
     public static void main(String[] args) throws IOException {
@@ -22,6 +20,8 @@ public class GiftDisCount {
         int count = Integer.parseInt(input[0]);
         long money = Long.parseLong(input[1]);
         int disCount = Integer.parseInt(input[2]);
+        long sum = 0;
+
         input = br.readLine().split(" ");
         int[] giftArray = new int[count];
         for (int i = 0; i < count; i++) {
@@ -29,7 +29,35 @@ public class GiftDisCount {
         }
 
         Arrays.sort(giftArray);
+        int start = 0; // 반값할인 시작지점
+        int end = 0; // 반값할인 종료지점
+        for (int i = 0; i < giftArray.length; i++) {
+            if (disCount > 0) {
+                // 할인 가능 횟수가 남아있는 경우
+                if (sum + (giftArray[i] / 2) <= money) {
+                    sum += (giftArray[i] / 2);
+                    disCount--;
+                    end = i + 1;
+                } else {
+                    // 할인 가능 횟수가 남아있는데
+                    // 반값할인을 하고도 현재 선물을 구매할 수 없는 경우
+                    break;
+                }
 
+            } else {
+                // 할인 가능 횟수가 남아있지 않은 경우
+                sum += (giftArray[start] / 2);
+                sum += giftArray[end] / 2;
+
+                if (sum > money) {
+                    break;
+                } else {
+                    start++;
+                    end++;
+                }
+            }
+        }
+        bw.write(end + "\n");
         bw.flush();
         bw.close();
         br.close();
