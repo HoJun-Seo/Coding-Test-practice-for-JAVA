7f1703d65d35bb088d173fd987594d1bf2d87fb0
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2023-04-29 17:55:08 +0900

    baekjoon

--- BackJoon/src/BackJoonQueue/Truck.java
+++ BackJoon/src/BackJoonQueue/Truck.java
@@ -24,20 +24,59 @@ public class Truck {
 
         Queue<TruckObject> queue = new LinkedList<>();
         input = br.readLine().split(" ");
-        for(int i = 0; i < count; i++){
+        for (int i = 0; i < count; i++) {
             TruckObject truckObject = new TruckObject(Integer.parseInt(input[i]), 0);
             queue.offer(truckObject);
         }
 
+        int currentWeight = 0;
+        // 각 반복마다 트럭들은 하나의 행동밖에 할 수 없다
+        // 이동, 이탈 둘중 하나
+        int result = 0;
+        while (!queue.isEmpty()) {
 
-    } 
+            result += 1;
+            int size = queue.size();
+            // 모든 트럭들에 대해 각 반복마다 행동 결정
+            for (int i = 0; i < size; i++) {
+                TruckObject truck = queue.poll();
+                // 아직 다리를 통과하지 않은 트럭이라면
+                if (truck.distance == 0) {
+                    // 다리위에 올라갈수 있다면
+                    if (currentWeight + truck.weight <= weight) {
+                        truck.distance += 1;
+                        currentWeight += truck.weight;
+                        queue.offer(truck);
+                    } else {
+                        // 현재 다리위에 올라갈 수 없다면
+                        queue.offer(truck);
+                    }
+                } else {
+                    // 다리위를 지나가고 있는 트럭이라면
+                    truck.distance += 1;
+
+                    if (truck.distance <= bridgeLength) {
+                        queue.offer(truck);
+                    } else {
+                        currentWeight -= truck.weight;
+                    }
+                }
+            }
+
+        }
+
+        bw.write(result + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
 
 class TruckObject {
     int weight;
     int distance;
 
-    public TruckObject(int weight, int distance){
+    public TruckObject(int weight, int distance) {
         this.weight = weight;
         this.distance = distance;
     }
