ab1aff3d25d0d2625e4eb4049038299fb3c46ba4
HoJun-Seo <ghwns6659@gmail.com>
2023-05-07 20:44:55 +0900

    baekjoon

--- BackJoon/src/BackJoonBinarySearch/DoubleArray.java
+++ BackJoon/src/BackJoonBinarySearch/DoubleArray.java
@@ -41,10 +41,42 @@ public class DoubleArray {
             for (int j = 0; j < n; j++) {
                 int number = a[j];
 
-                
+                boolean check = false;
+                int start = 0;
+                int end = m - 1;
+                while (start + 1 < end) {
+                    int mid = (start + end) / 2;
+
+                    // 중간에 number 와 일치하는 값을 찾았을 경우
+                    if (number == b[mid]) {
+                        check = true;
+                        c[j] = number;
+                        break;
+                    } else if (number > b[mid]) {
+                        start = mid;
+                    } else if (number < b[mid]) {
+                        end = mid;
+                    }
+                }
+                // 반복문이 모두 끝났을 경우 start 인덱스 값은 number 보다 작은 값 중에서 가장 큰값
+                // end 인덱스 값은 number 보다 큰 값 중에서 가장 작은 값에 해당한다.
+                if (!check) {
+                    if (Math.abs(number - b[start]) == Math.abs(number - b[end])) {
+                        c[j] = b[start];
+                    } else if (Math.abs(number - b[start]) < Math.abs(number - b[end])) {
+                        c[j] = b[start];
+                    } else if (Math.abs(number - b[start]) > Math.abs(number - b[end])) {
+                        c[j] = b[end];
+                    }
+                }
             }
 
-            bw.write(Arrays.stream(c).sum() + "\n");
+            // 각 값의 최대값이 10의 9제곱이 될 수 있으므로 합산 할 경우 long 자료형으로 지정해줘야 한다.
+            long result = 0;
+            for(int j = 0; j < c.length; j++){
+                result += c[j];
+            }
+            bw.write(result + "\n");
         }
 
         bw.flush();
