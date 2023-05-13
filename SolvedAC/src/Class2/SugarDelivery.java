ff9595710e8a5862d31cb4de92b300d879a2d89c
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2023-05-13 17:28:57 +0900

    baekjoon

--- SolvedAC/src/Class2/SugarDelivery.java
+++ SolvedAC/src/Class2/SugarDelivery.java
@@ -8,44 +8,51 @@ import java.io.OutputStreamWriter;
 
 public class SugarDelivery {
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		new SugarDelivery().solution();
+	}
+
+	private void solution() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int[] sugar = new int[5000];
-		sugar[0] = 1; sugar[1] = -1; sugar[2] = 1; sugar[3] = 2; sugar[4] = -1;
-		sugar[5] = 2; sugar[6] = 3; sugar[7] = 2; sugar[8] = 3;
-		sugar[10] = 3; sugar[12] = 3;
-		
-		int evenCount = 5;
-		int oddCount = 5;
-		for(int i = 9; i < sugar.length; i++) {
-			if(i % 2 == 1) {
-				if(oddCount == 5) {
-					oddCount = 1;
-					sugar[i] = sugar[i-2] + 2;
-				}else {
-					sugar[i] = sugar[i-2];
-					oddCount++;
+
+		int n = Integer.parseInt(br.readLine());
+
+		// 5킬로그램, 3킬로그램 봉지 조합으로 n킬로그램을 전달할 수 있는지 여부 검사
+		boolean check = false;
+		// 5킬로그램 짜리 봉지 갯수, 각 갯수별로 3킬로그램 봉지 특정 갯수와 조합해서 n 킬로그램을 만들 수 있는지 검사한다.
+		// 만들 수 있는 경우, 만들 수 없는 경우 상관없이 매 반복마다 1씩 추가해줄것
+		// n킬로그램을 만들 수 있는 모든 조합을 찾아서 그 중 가장 작은값을 얻어야 하기 때문
+		int fiveCount = 0;
+		// 5킬로그램 봉지의 갯수를 계속 늘려 나가다가 5킬로그램 봉지만으로 n킬로그램을 넘겨버리면 반복문 종료
+		int min = Integer.MAX_VALUE;
+		while (5 * fiveCount <= n) {
+			int spare = n - (5 * fiveCount);
+
+			// 5킬로그램 짜리 봉지만으로 n킬로그램을 만들 수 없는 경우
+			if (spare > 0) {
+				int threeCount = spare / 3;
+				int change = spare % 3;
+				// 3킬로그램 봉지를 조합해도 n킬로그램을 만들 수 없는 경우
+				if (change > 0) {
+					fiveCount++;
+				} else if (change == 0) {
+					// 3킬로그램 봉지까지 조합해서 n킬로그램을 만들 수 있는 경우
+					min = Math.min(min, fiveCount + threeCount);
+					fiveCount++;
+					check = true;
 				}
+			} else if (spare == 0) {
+				// 5킬로그램 짜리 봉지만으로 n킬로그램을 만들었을 경우
+				min = Math.min(min, fiveCount);
+				fiveCount++;
+				check = true;
 			}
-			else if(i % 2 == 0) {
-				if(i == 10 || i == 12)
-					continue;
-				else {
-					if(evenCount == 5) {
-						evenCount = 1;
-						sugar[i] = sugar[i-2] + 2;
-					}
-					else {
-						sugar[i] = sugar[i-2];
-						evenCount++;
-					}	
-				}
-			}	
 		}
-		
-		int number = Integer.parseInt(br.readLine());
-		bw.write(sugar[number-3] + "\n");
+		if (check) {
+			bw.write(min + "\n");
+		} else {
+			bw.write(-1 + "\n");
+		}
 		bw.flush();
 		bw.close();
 	}
