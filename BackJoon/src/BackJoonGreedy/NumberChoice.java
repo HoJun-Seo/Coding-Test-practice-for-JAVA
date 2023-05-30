2a82c22d2166a3251131d6d004112601cd9cf699
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2023-05-30 21:33:19 +0900

    baekjoon

--- BackJoon/src/BackJoonGreedy/NumberChoice.java
+++ BackJoon/src/BackJoonGreedy/NumberChoice.java
@@ -6,8 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Collections;
 
 public class NumberChoice {
     public static void main(String[] args) throws IOException {
@@ -22,47 +21,22 @@ public class NumberChoice {
         int size = Integer.parseInt(input[0]);
         int count = Integer.parseInt(input[1]);
 
-        Number[] numberArray = new Number[size];
-        Queue<Number> queue = new LinkedList<>();
+        Integer[] numberArray = new Integer[size];
 
         input = br.readLine().split(" ");
         for (int i = 0; i < input.length; i++) {
-            Number number = new Number(i, Integer.parseInt(input[i]));
-            numberArray[i] = number;
-        }
-        Number[] cloneArray = numberArray.clone();
-        Arrays.sort(numberArray);
-        for (int i = 0; i < count; i++) {
-            queue.offer(numberArray[i]);
+            numberArray[i] = Integer.parseInt(input[i]);
         }
 
-        int[] indexArray = new int[count];
-        for (int i = 0; i < indexArray.length; i++) {
-            indexArray[i] = queue.poll().index;
-        }
-        Arrays.sort(indexArray);
+        Arrays.sort(numberArray, Collections.reverseOrder());
+
         int result = 0;
-        for (int i = 0; i < indexArray.length; i++) {
-            result += (cloneArray[indexArray[i]].value - i);
+        for (int i = 0; i < count; i++) {
+            result += (numberArray[i] - i);
         }
 
         bw.write(result + "\n");
         bw.flush();
         bw.close();
-    }
-}
-
-class Number implements Comparable<Number> {
-    int index;
-    int value;
-
-    public Number(int index, int value) {
-        this.index = index;
-        this.value = value;
-    }
-
-    @Override
-    public int compareTo(Number arg0) {
-        return arg0.value - this.value;
     }
 }
