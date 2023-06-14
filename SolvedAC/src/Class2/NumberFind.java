10f561b5c819a2918e8135484a6bf216b8c1da69
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2023-06-14 20:23:25 +0900

    baekjoon

--- SolvedAC/src/Class2/NumberFind.java
+++ SolvedAC/src/Class2/NumberFind.java
@@ -5,32 +5,56 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
+import java.util.Arrays;
 
 public class NumberFind {
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		new NumberFind().solution();
+	}
+
+	private void solution() throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		TreeSet<Integer> numberList = new TreeSet<Integer>();
-		int count = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		for(int i = 0; i < count; i++) {
-			
-			numberList.add(Integer.parseInt(st.nextToken()));
+
+		int size = Integer.parseInt(br.readLine());
+		int[] numberArray = new int[size];
+		String[] input = br.readLine().split(" ");
+		for (int i = 0; i < input.length; i++) {
+			numberArray[i] = Integer.parseInt(input[i]);
 		}
-		int testCount = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine(), " ");
-		for(int i = 0; i < testCount; i++) {
-			if(numberList.contains(Integer.parseInt(st.nextToken()))) {
-				bw.write(1 + "\n");
+
+		Arrays.sort(numberArray);
+
+		size = Integer.parseInt(br.readLine());
+		input = br.readLine().split(" ");
+		for (int i = 0; i < size; i++) {
+			int numberX = Integer.parseInt(input[i]);
+
+			int start = 0;
+			int end = numberArray.length - 1;
+			boolean find = false;
+			while (start <= end) {
+				int mid = (start + end) / 2;
+
+				if (numberArray[mid] == numberX) {
+					find = true;
+					break;
+				} else if (numberArray[mid] > numberX) {
+					end = mid - 1;
+				} else {
+					start = mid + 1;
+				}
 			}
-			else {
+
+			if (find) {
+				bw.write(1 + "\n");
+			} else {
 				bw.write(0 + "\n");
 			}
 		}
+
 		bw.flush();
 		bw.close();
+		br.close();
 	}
 }
