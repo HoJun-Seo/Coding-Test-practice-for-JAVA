930e5824835a82a7f63abe4a0e3de74c77a8d863
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2023-06-27 21:08:14 +0900

    baekjoon

--- SolvedAC/src/Class2/CutTree.java
+++ SolvedAC/src/Class2/CutTree.java
@@ -5,47 +5,58 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
 
 public class CutTree {
 	public static void main(String[] args) throws IOException {
+		new CutTree().solution();
+	}
+
+	private void solution() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		List<Integer> treeList = new ArrayList<Integer>();
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int treeCount = Integer.parseInt(st.nextToken());
-		int height = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine(), " ");
-		for(int i = 0; i < treeCount; i++) {
-			treeList.add(Integer.parseInt(st.nextToken()));
+
+		String[] input = br.readLine().split(" ");
+		int n = Integer.parseInt(input[0]);
+		long m = Long.parseLong(input[1]); // 가져가려고 하는 나무의 길이
+
+		input = br.readLine().split(" ");
+		int[] treeArray = new int[n];
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < treeArray.length; i++) {
+			treeArray[i] = Integer.parseInt(input[i]);
+			max = Math.max(max, treeArray[i]);
 		}
-		
-		int end = Collections.max(treeList) + 1;
+
 		int start = 0;
-		
-		while(end > start) {
-			int middle = (start + end) / 2;
-			
-			long cutSize = 0; // 잘려진 나무의 길이를 합산할 시 int 형의 표현범위를 넘어가는 경우가 발생하여 자료형 교체
-			for(int i = 0; i < treeList.size(); i++) {
-				int tree = treeList.get(i);
-				if(tree > middle)
-					cutSize += tree - middle;
+		int end = max;
+		long result = 0;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+
+			long sum = 0;
+			for (int i = 0; i < treeArray.length; i++) {
+
+				// 나무의 길이가 mid 보다 작으면 둘을 뺄셈한 값이 음수가 될 수 있으므로
+				// 나무의 길이가 mid 보다 크거나 같은 경우에만 뺄셈을 수행해준다.
+				if (treeArray[i] >= mid) {
+					sum += (treeArray[i] - mid);
+				}
 			}
-			
-			if(cutSize >= height)
-				start = middle + 1;
-			else
-				end = middle;
+
+			// m 이상으로 나무를 가져갈 수 있는 경우 톱의 높이를 더 높여본다.
+			if (sum >= m) {
+				result = mid;
+				start = mid + 1;
+			} else {
+				// m 이상으로 나무를 가져갈 수 없는 경우 톱의 높이를 낮춰서
+				// 다음 반복에서 가져갈 수 있는 나무의 길이를 더 늘려주자.
+				end = mid - 1;
+			}
 		}
-		
-		bw.write(end-1 + "\n");
+
+		bw.write(result + "\n");
 		bw.flush();
 		bw.close();
+		br.close();
 	}
 }
--- SolvedAC/src/Class2/LANlineCut.java
+++ SolvedAC/src/Class2/LANlineCut.java
@@ -8,46 +8,51 @@ import java.io.OutputStreamWriter;
 
 public class LANlineCut {
 	public static void main(String[] args) throws IOException {
+		new LANlineCut().solution();
+	}
+
+	private void solution() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		String[] inputArray = br.readLine().split(" ");
 		int curline = Integer.parseInt(inputArray[0]);
 		int reqline = Integer.parseInt(inputArray[1]);
 		long[] lineArray = new long[curline];
 
 		long max = 0;
-		for(int i = 0; i < curline; i++){
+		for (int i = 0; i < curline; i++) {
 
 			long length = Long.parseLong(br.readLine());
 			lineArray[i] = length;
-			if(max < length)
+			if (max < length)
 				max = length;
 		}
 
 		long start = 1;
-		long end = max+1;
+		long end = max;
 		long mid = 0;
-		while(start+1 < end){
+		long result = 0;
+		while (start <= end) {
 			mid = (start + end) / 2;
 
 			long sum = 0;
-			for(int i = 0; i < lineArray.length; i++){
+			for (int i = 0; i < lineArray.length; i++) {
 				long line = lineArray[i];
 
-				long cutCount = line/mid;
+				long cutCount = line / mid;
 				sum += cutCount;
 			}
 
-			if(sum < reqline){
-				end = mid;
-			}
-			else{
-				start = mid;
+			if (sum < reqline) {
+				end = mid - 1;
+			} else {
+				result = mid;
+				start = mid + 1;
 			}
 		}
 
-		bw.write(start + "\n");
+		bw.write(result + "\n");
 		bw.flush();
 		bw.close();
 		br.close();
