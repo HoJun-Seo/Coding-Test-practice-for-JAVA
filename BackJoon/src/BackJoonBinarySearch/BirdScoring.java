5775fe8779c3fdd0af16079fa3109ed3b24e93a7
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2023-07-07 21:42:38 +0900

    baekjoon

--- BackJoon/src/BackJoonBinarySearch/BirdScoring.java
+++ BackJoon/src/BackJoonBinarySearch/BirdScoring.java
@@ -5,8 +5,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.PriorityQueue;
 
 public class BirdScoring {
     public static void main(String[] args) throws NumberFormatException, IOException {
@@ -19,75 +17,47 @@ public class BirdScoring {
 
         int n = Integer.parseInt(br.readLine());
 
-        int[] aArray = new int[n];
-        int[] bArray = new int[n];
-
-        String[] input = br.readLine().split(" ");
-        for(int i = 0; i < aArray.length; i++){
-            aArray[i] = Integer.parseInt(input[i]);
+        String[] inputA = br.readLine().split(" ");
+        String[] inputB = br.readLine().split(" ");
+        // 장애물 틈새값들을 저장하기 위한 배열 선언 및 틈새값 저장
+        int[] termArray = new int[n];
+        for (int i = 0; i < termArray.length; i++) {
+            termArray[i] = Integer.parseInt(inputA[i]) - Integer.parseInt(inputB[i]);
+        }
+        // 단조감소 처리
+        for (int i = 1; i < termArray.length; i++) {
+            if (termArray[i] > termArray[i - 1]) {
+                termArray[i] = termArray[i - 1];
+            }
+        }
+        // 단조감소시 내림차순으로 정렬되므로 각 항에 -1을 곱하여 오름차순 정렬로 변형
+        for (int i = 0; i < termArray.length; i++) {
+            termArray[i] *= -1;
         }
 
-        input = br.readLine().split(" ");
-        for(int i = 0; i < bArray.length; i++){
-            bArray[i] = Integer.parseInt(input[i]);
-        }
-
-        Bird[] birds = new Bird[n];
-        for(int i = 0; i < n; i++){
-            int term = aArray[i] - bArray[i];
-            birds[i] = new Bird(term, i);
-        }
-        Arrays.sort(birds);
-
+        // 오름차순으로 변형된 배열을 대상으로 이분탐색을 수행하여 최초로 현재 birdSize * -1 한 값 보다
+        // 큰 값이 나타나는 위치를 찾는다.(UpperBound)
         int birdCount = Integer.parseInt(br.readLine());
-        input = br.readLine().split(" ");
-        for(int i = 0; i < birdCount; i++){
-            int birdSize = Integer.parseInt(input[i]);
-            // lowerBound
+        String[] inputBird = br.readLine().split(" ");
+        for (int i = 0; i < birdCount; i++) {
+            int birdSize = Integer.parseInt(inputBird[i]) * -1;
+
             int start = 0;
             int end = n;
-            while(start < end){
+            while (start < end) {
                 int mid = (start + end) / 2;
 
-                if(birdSize <= birds[mid].term){
-                    end = mid;
-                } else {
+                if (birdSize >= termArray[mid]) {
                     start = mid + 1;
+                } else {
+                    end = mid;
                 }
             }
 
-            //  birdSize 보다 작은 값들 중 가장 작은 인덱스 찾기
-            int min = Integer.MAX_VALUE;
-            for(int index = 0; index < start; index++){
-                min = Math.min(min, birds[index].index);
-            }
-
-            if(min == Integer.MAX_VALUE){
-                // 이곳에 있는 코드가 실행된다면 현재 birdSize 보다 틈의 크기가 작은 장애물이 없다는 뜻
-                // 즉, 장애물 끝까지 갈 수 있다.
-                bw.write(n + "\n");
-            } else {
-                bw.write(min + "\n");
-            }
-        } 
-
+            bw.write(start + "\n");
+        }
         bw.flush();
         bw.close();
         br.close();
-    }
-}
-
-class Bird implements Comparable<Bird>{
-    int term;
-    int index;
-
-    public Bird(int term, int index){
-        this.term = term;
-        this.index = index;
-    }
-
-    @Override
-    public int compareTo(Bird o) {
-        return this.term - o.term;
     }
 }
