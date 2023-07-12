8a6d3ea0407b0ba9d2d49bb48e6f5a63888bdbf9
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2023-07-12 17:02:30 +0900

    baekjoon

--- SolvedAC/src/Class3/Virus.java
+++ SolvedAC/src/Class3/Virus.java
@@ -5,76 +5,78 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Virus {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		HashMap<Integer, List<Integer>> hashmapBFS = new HashMap<Integer, List<Integer>>();
-		
-		int nodeCount = Integer.parseInt(br.readLine());
-		int edge = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < edge; i++) {
-			
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int key = Integer.parseInt(st.nextToken());
-			int connectNode = Integer.parseInt(st.nextToken());
-			
-			List<Integer> keyList = new ArrayList<Integer>();
-			List<Integer> connectList = new ArrayList<Integer>();
-			
-			if(hashmapBFS.containsKey(key)) {
-				connectList = hashmapBFS.get(key);
-				connectList.add(connectNode);
-				hashmapBFS.replace(key, connectList);
-			}
-			else {
-				connectList.add(connectNode);
-				hashmapBFS.put(key, connectList);
-			}
-			
-			if(hashmapBFS.containsKey(connectNode)) {
-				keyList = hashmapBFS.get(connectNode);
-				keyList.add(key);
-				hashmapBFS.replace(connectNode, keyList);
-			}
-			else {
-				keyList.add(key);
-				hashmapBFS.put(connectNode, keyList);
-			}
-		}
-		
-		Queue<Integer> visited = bfs(hashmapBFS, 1);
-		
-		// 1번 컴퓨터는 제외 시켜야함
-		bw.write(visited.size()-1 + "\n");
-		bw.flush();
-		bw.close();
+		new Virus().solution();
 	}
 
-	private static Queue<Integer> bfs(HashMap<Integer, List<Integer>> hashmapBFS, int startNode) {
-		
-		Queue<Integer> needVisit = new LinkedList<Integer>();
-		Queue<Integer> visited = new LinkedList<Integer>();
-		
-		needVisit.add(startNode);
-		
-		while(!needVisit.isEmpty()) {
-			int node = needVisit.poll();
-			if(!visited.contains(node)) {
-				visited.offer(node);
-				needVisit.addAll(hashmapBFS.get(node));
+	static HashMap<Integer, Queue<Integer>> network = new HashMap<>();
+	static boolean[] visited;
+	static int count = 0;
+
+	private void solution() throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+		visited = new boolean[n + 1];
+		int connect = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < connect; i++) {
+			String[] input = br.readLine().split(" ");
+			int a = Integer.parseInt(input[0]);
+			int b = Integer.parseInt(input[1]);
+
+			Queue<Integer> queueA = new LinkedList<>();
+			Queue<Integer> queueB = new LinkedList<>();
+			if (network.containsKey(a)) {
+				queueA = network.get(a);
 			}
+			queueA.offer(b);
+			network.put(a, queueA);
+
+			if (network.containsKey(b)) {
+				queueB = network.get(b);
+			}
+			queueB.offer(a);
+			network.put(b, queueB);
 		}
 
-		return visited;
+		bfs(1);
+
+		bw.write(count + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	private void bfs(int i) {
+		Queue<Integer> needVisit = new LinkedList<>();
+		needVisit.offer(i);
+		visited[i] = true;
+
+		while (!needVisit.isEmpty()) {
+			int computer = needVisit.poll();
+			if (computer != i) {
+				count++;
+			}
+			Queue<Integer> queue = new LinkedList<>();
+			if (network.containsKey(computer)) {
+				queue = network.get(computer);
+			}
+			if (queue.size() > 0) {
+				while (!queue.isEmpty()) {
+					int connectCom = queue.poll();
+					if (!visited[connectCom]) {
+						visited[connectCom] = true;
+						needVisit.offer(connectCom);
+					}
+				}
+			}
+		}
 	}
 }
