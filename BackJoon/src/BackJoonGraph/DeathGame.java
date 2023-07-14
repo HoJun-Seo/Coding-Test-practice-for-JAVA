7e429b040f4e30d89cf1a02478319c8a0576c355
HoJun-Seo <ghwns6659@gmail.com>
2023-07-14 23:46:19 +0900

    baekjoon

---
+++ BackJoon/src/BackJoonGraph/DeathGame.java
@@ -0,0 +1,17 @@
+package BackJoonGraph;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class DeathGame {
+    public static void main(String[] args) {
+        new DeathGame().solution();
+    }
+
+    private void solution() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+}
