1c3c66f420965901a4eacafc1fba05f6ed98cd94
HoJun-Seo <48443312+HoJun-Seo@users.noreply.github.com>
2023-07-20 20:16:39 +0900

    baekjoon

--- BackJoon/src/BackJoonGraph/NumberBoardJump.java
+++ BackJoon/src/BackJoonGraph/NumberBoardJump.java
@@ -11,63 +11,57 @@ import java.util.Set;
 public class NumberBoardJump {
 
     static char[][] graph;
-    static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {1, 0, -1, 0};
+    static int[] dx = { 0, 1, 0, -1 };
+    static int[] dy = { 1, 0, -1, 0 };
     static Set<String> route = new HashSet<>();
+
     public static void main(String[] args) throws IOException {
+        new NumberBoardJump().solution();
+    }
+
+    private void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         graph = new char[5][5];
 
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             String[] inputArray = br.readLine().split(" ");
 
-            for(int j = 0; j < 5; j++){
+            for (int j = 0; j < 5; j++) {
                 graph[i][j] = inputArray[j].charAt(0);
             }
         }
 
         String str = "";
-        for(int i = 0; i < 5; i++){
-            for(int j = 0; j < 5; j++){
-                // 여기서 각 요소를 시작점으로 DFS 탐색 후 만들어진 6자리 수 중에서 중복은 배제시킴
-                // 중복 배제는 Set 자료구조를 활용하면 됨
-                for(int k = 0; k < 4; k++){
-
-                    int ii = i + dx[k];
-                    int jj = j + dy[k];
-                    if(ii < 0 || ii == 5 || jj < 0 || jj == 5) {
-                        continue;
-                    }
-                    else{
-                        // 반복문을 통해 각 방향을 기준으로 재귀 DFS 탐색 시작
-                        dfs(ii, jj, str+graph[ii][jj]);
-                    }
-                }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                dfs(i, j, str + graph[i][j]);
             }
         }
 
-        bw.write(route.size() + "\n");      
+        bw.write(route.size() + "\n");
         bw.flush();
         bw.close();
         br.close();
     }
+
     private static void dfs(int ii, int jj, String str) {
 
-        if(str.length() == 6){
+        // 여기서 각 요소를 시작점으로 DFS 탐색 후 만들어진 6자리 수 중에서 중복은 배제시킴
+        // 중복 배제는 Set 자료구조를 활용하면 됨
+        if (str.length() == 6) {
             route.add(str);
-        }
-        else{
-            for(int i = 0; i < 4; i++){
+        } else {
+            for (int i = 0; i < 4; i++) {
                 int di = ii + dx[i];
                 int dj = jj + dy[i];
 
-                if(di < 0 || di == 5 || dj < 0 || dj == 5){
+                if (di < 0 || di == 5 || dj < 0 || dj == 5) {
                     continue;
-                }
-                else{
-                    dfs(di, dj, str+graph[di][dj]);
+                } else {
+                    // 반복문을 통해 각 방향을 기준으로 재귀 DFS 탐색 시작
+                    dfs(di, dj, str + graph[di][dj]);
                 }
             }
         }
